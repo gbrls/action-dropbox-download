7180676aea962d1cded373d799ecba23a8f9e305
Gabriel Schneider <gas6@cin.ufpe.br>
2022-08-20 17:42:55 -0300

    cleaning up files

--- scripts/fetch-api.sh
+++ scripts/fetch-api.sh
@@ -5,7 +5,6 @@ cd .dropbox || true
 mix local.rebar --force 
 mix local.hex --force 
 elixir scripts/main.exs
-unzip data.zip -d $OUT_PATH
+unzip data.zip -d ../$OUT_PATH
 
-# @TODO: Cleanup this repo's files afterwards
-
+rm -rf ../.dropbox || true
